f250ca3ad925a83e0864bcbf13cf9fd227d6b0e4
Nadya Hapsari Putri <nadya.hp14@gmail.com>
2025-02-26 21:50:11 +0700

    sixth commit

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
-
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
