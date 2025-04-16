5db2611c3e0b1ec275a309f6a8fb5d4cddbc6ce1
Nadya Hapsari Putri <nadya.hp14@gmail.com>
2025-04-16 09:58:52 +0700

    Jobsheet 5_Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,57 @@
 <?php
 
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Requests\StorePostRequest;
-use App\Http\Controllers\POSController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index'])->name('user.index');
-Route::get('/user/tambah', [UserController::class, 'tambah'])->name('user.tambah');
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('user.tambah_simpan');
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('user.ubah');
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah_simpan');
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('user.hapus');
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/kategori/create', [KategoriController::class, 'create']);
-Route::post('/kategori', [KategoriController::class, 'store']);
-
-// Kategori routes
-Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
-Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
-Route::post('/kategori', [KategoriController::class, 'store'])->name('kategori.store');
-Route::get('/kategori/{id}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
-Route::put('/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
-Route::resource('kategori', KategoriController::class);
-Route::delete('/kategori/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
-
-Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
-Route::post('/kategori', [KategoriController::class, 'store'])->name('kategori.store');
-
-Route::resource('users', POSController::class);
+// use App\Http\Controllers\KategoriController;
+// use App\Http\Controllers\LevelController;
+// use App\Http\Controllers\UserController;
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Requests\StorePostRequest;
+// use App\Http\Controllers\POSController;
 
 use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);        // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);    // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']); // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);       // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);     // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);   // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/user', [UserController::class, 'index']);
+// Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/user', [UserController::class, 'index'])->name('user.index');
+// Route::get('/user/tambah', [UserController::class, 'tambah'])->name('user.tambah');
+// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('user.tambah_simpan');
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('user.ubah');
+// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah_simpan');
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('user.hapus');
+// Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/kategori/create', [KategoriController::class, 'create']);
+// Route::post('/kategori', [KategoriController::class, 'store']);
+
+// // Kategori routes
+// Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
+// Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
+// Route::post('/kategori', [KategoriController::class, 'store'])->name('kategori.store');
+// Route::get('/kategori/{id}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
+// Route::put('/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
+// Route::resource('kategori', KategoriController::class);
+// Route::delete('/kategori/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
+
+// Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
+// Route::post('/kategori', [KategoriController::class, 'store'])->name('kategori.store');
+
+// Route::resource('users', POSController::class);
+
