654c4f485d64575795329e1a004f34d79b85fd44
Nadya Hapsari Putri <nadya.hp14@gmail.com>
2025-04-30 19:21:52 +0700

    Jobsheet 10_Tugas

---
+++ app/Http/Controllers/Api/BarangController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\BarangModel;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BarangController extends Controller
+{
+    public function index()
+    {
+        return BarangModel::all();
+    }
+
+    public function store(Request $request)
+    {
+        return BarangModel::create($request->all());
+    }
+
+    public function show($id)
+    {
+        return BarangModel::find($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = BarangModel::find($id);
+        $data->update($request->all());
+        return $data;
+    }
+
+    public function destroy(BarangModel $barang)
+    {
+        $barang->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data terhapus',
+        ]);
+    }
+}
