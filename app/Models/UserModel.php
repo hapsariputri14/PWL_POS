85b14568a3a0ffdd73efe4be8895509a6e180b61
Nadya Hapsari Putri <nadya.hp14@gmail.com>
2025-04-30 12:32:16 +0700

    Jobsheet 10_Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,58 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
-    protected $hidden = ['password']; // jangan di tampilkan saat select
-    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
-    /**
-     * Relasi ke tabel level
-     */
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
-
-    /**
-     * Mendapatkan nama role
-     */
-    public function getRoleName(): string
-    {
-        return $this->level->level_nama;
-    }
-
-    /**
-     * Cek apakah user memiliki role tertentu
-     */
-    public function hasRole($role): bool
-    {
-        return $this->level->level_kode == $role;
-    }
-
-    /**
-     * Mendapatkan kode role
-     */
-    public function getRole(): string
-    {
-        return $this->level->level_kode;
-    }
-    
-    public function getPhotoUrlAttribute()
-    {
-        if ($this->photo) {
-            return asset('storage/profile_photos/' . $this->photo);
-        }
-        
-        return asset('adminlte/dist/img/user2-160x160.jpg');
-    }
 }
+
